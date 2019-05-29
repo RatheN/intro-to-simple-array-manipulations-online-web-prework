560e082b29c1f90c676fce9151ea475988f2688d
Nathan Rathe <nprathe10@gmail.com>
2019-05-29 23:19:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,6 @@ def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties.pop
 end
 
-def pop_with_args(chars_in_game_of_thrones)
+def pop_with_args(chars_in_game_of_thrones, chars_arya_killed)
   chars_in_game_of_thrones.pop(chars_arya_killed)
 end
